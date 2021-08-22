d481ea4ebb160b216ffff32adbee60e539860661
andrey <andreyten2006@gmail.com>
2021-08-22 15:37:46 +0500

    JS

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-alert('Задание 1');
+/* alert('Задание 1');
 
 
-let num = prompt('Введите число');
+
+do {
+    var num = +prompt('Введите число');
+} while (num <= 0 || isNaN(num));
+
+do {
+    var num2 = prompt('Введите степень числа');
+} while (num <= 0 || isNaN(num2)){
+    if(isNaN(num)){
+        num2 = 2;
+    }
+}
+
 let num1 = num;
-let num2 = prompt('Введите степень числа');
 
 for(let i = 1; i < num2; i++) {
     num = num * num1;
 }
 
-alert( num );
+alert( num ); */
 
 
 
@@ -27,19 +38,19 @@ do {
     if(isNaN(count)){
         count = 5;
     }
-    console.log('Количество ступенек: ' + count);
+    /* console.log('Количество ступенек: ' + count); */
 }
 
 do {
-    var space = prompt('Введите символ отступов');
+    var space = prompt('Введите символ отступов (буква или знак)');
 } while (!isNaN(space)){
-    console.log('Cимвол отступов: ' + space);
+    /* console.log('Cимвол отступов: ' + space); */
 } 
 
 do {
-    var end = prompt('Введите конечный символ');
+    var end = prompt('Введите конечный символ (буква или знак)');
 } while (!isNaN(end)){
-    console.log('Конечный символ: ' + end);
+    /* console.log('Конечный символ: ' + end); */
 } 
 
 var space1 = '';
@@ -49,3 +60,5 @@ for(let i = 0; i < count; i++){
     console.log(space1 + end);
     space1 = space + space1;
 }
+
+alert('Зайдите в консоль')
